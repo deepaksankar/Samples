062d44072c68345d409d11a60f452c4bf3fee614
Deepak Sankar <sdeepakishere@gmail.com>
2019-03-17 12:34:45 -0500

    Adding word break problem to git

---
+++ src/main/java/Leetcode/medium/WordBreakii.java
@@ -0,0 +1,42 @@
+package main.java.Leetcode.medium;
+
+import java.util.*;
+
+public class WordBreakii {
+    public static void main(String[] args) {
+        String s = "catsanddog";
+        String[] words = {"cat", "and", "sand", "dog"};
+        Set<String> wordSet = new LinkedHashSet<>();
+
+        for (String word : words) {
+            wordSet.add(word);
+        }
+
+        List<String> output = dfs(s, wordSet, new HashMap<>());
+        System.out.println("Done");
+    }
+
+    static List<String> dfs(String s, Set<String> wordSet, Map<String, List<String>> map) {
+        if(map.containsKey(s)) {
+            return map.get(s);
+        }
+
+        List<String> res = new LinkedList<>();
+        if(s.length() == 0) {
+            res.add("");
+            return res;
+        }
+
+        for (String word : wordSet) {
+            if(s.startsWith(word)) {
+                List<String> subStr = dfs(s.substring(word.length()), wordSet, map);
+                for (String s1 : subStr) {
+                    res.add(word + (s1.isEmpty() ? "" : " " + s1));
+                }
+            }
+        }
+        map.put(s, res);
+
+        return res;
+    }
+}
