5674f5e2ca79f5160ae65de5facb4f7f034bd484
Deepak Sankar <sdeepakishere@gmail.com>
2019-03-22 17:01:28 -0500

    Adding largest sub matrix shell to git

---
+++ src/main/java/GFG/LargestSubMatrix.java
@@ -0,0 +1,10 @@
+package main.java.GFG;
+
+/**
+ * Given a matrix of 1's and 0's, find the largest sub matrix that only contains 1
+ */
+public class LargestSubMatrix {
+    public static void main(String[] args) {
+
+    }
+}
