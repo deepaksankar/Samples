772466f73d5c9356908be7d1d6ae83e6b153af5c
Deepak Sankar <sdeepakishere@gmail.com>
2019-03-30 12:35:23 -0500

    Adding meeting rooms problem to leetcode easy package

---
+++ src/main/java/Leetcode/easy/MeetingRooms.java
@@ -0,0 +1,28 @@
+package main.java.Leetcode.easy;
+
+import java.util.Arrays;
+
+public class MeetingRooms {
+    public static void main(String[] args) {
+        
+    }
+
+    static boolean canScheduleMeetings(Interval[] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i = 0; i < intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        for(int i = 0; i < starts.length-1; i++) {
+            if(starts[i+1] > starts[i] && starts[i+1] < ends[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
