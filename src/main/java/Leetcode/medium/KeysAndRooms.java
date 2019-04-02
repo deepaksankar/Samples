408cd038c3b96977ecade1bcff67b6e2b9aa5937
Deepak Sankar <sdeepakishere@gmail.com>
2019-04-02 08:24:06 -0500

    Adding keys and rooms problem to leetcode medium package

---
+++ src/main/java/Leetcode/medium/KeysAndRooms.java
@@ -0,0 +1,30 @@
+package main.java.Leetcode.medium;
+
+import java.util.*;
+
+/**
+ * Given a list of rooms and keys to the rooms, find if you can visit all the rooms.
+ */
+public class KeysAndRooms {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> isVisited = new LinkedHashSet<>();
+        isVisited.add(0);
+        Stack<Integer> room = new Stack<>();
+        room.add(0);
+
+        while(!room.isEmpty()) {
+            List<Integer> keys = new ArrayList<>();
+            for(int key : keys) {
+                if(!isVisited.contains(rooms.get(key))) {
+                    room.add(key);
+                    isVisited.add(key);
+                }
+            }
+        }
+        return isVisited.size() == rooms.size();
+    }
+}
