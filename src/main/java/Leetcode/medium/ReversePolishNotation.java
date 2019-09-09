3839d99050aa4dfbc6fab07c0fc3f0d3491d1b3c
Deepak <sdeepakishere@gmail.com>
2019-09-08 20:37:20 -0500

    Solving polish notation and returning result

---
+++ src/main/java/Leetcode/medium/ReversePolishNotation.java
@@ -0,0 +1,43 @@
+package main.java.Leetcode.medium;
+
+import java.util.Stack;
+
+public class ReversePolishNotation {
+    public static void main(String[] args) {
+        String[] input = new String[]{"2", "1", "+", "3", "*"};
+        int result = evalRPN(input);
+        System.out.println(result);
+    }
+    static int evalRPN(String[] tokens) {
+        if(tokens == null || tokens.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+
+        for(int i = 0; i < tokens.length; i++) {
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch(tokens[i]) {
+                    case "+":
+                        result = a+b;
+                        break;
+                    case "-":
+                        result = a-b;
+                        break;
+                    case "*":
+                        result = a*b;
+                        break;
+                    case "/":
+                        result = a/b;
+                        break;
+                }
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
