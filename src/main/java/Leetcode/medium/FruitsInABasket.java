1338b6d52917fbd156ac6e2ef148fc51d29548f3
Deepak <sdeepakishere@gmail.com>
2019-09-14 15:58:08 -0500

    Moving logic into its own function

--- src/main/java/Leetcode/medium/FruitsInABasket.java
+++ src/main/java/Leetcode/medium/FruitsInABasket.java
@@ -1,37 +1,39 @@
 package main.java.Leetcode.medium;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class FruitsInABasket {
     public static void main(String[] args) {
-        int[] trees = {};
-
-        if(trees == null || trees.length == 0) {
-            return;
+        int[] trees = {3,3,3,1,2,1,1,2,3,3,4};
+        int count = totalFruit(trees);
+        System.out.println(count);
+    }
+    static int totalFruit(int[] tree) {
+        if(tree == null || tree.length == 0) {
+            return 0;
         }
-
         int max = 1;
+        Map<Integer, Integer> fruitMap = new HashMap<>();
 
-        Map<Integer, Integer> mapping = new LinkedHashMap<>();
-        int start = 0;
-        int current = 0;
+        int j = 0; int i = 0;
 
-        while (current < trees.length) {
-            if(mapping.size() <= 2) {
-                mapping.put(trees[current], current++);
+        while(j < tree.length) {
+            if(fruitMap.size() <= 2) {
+                fruitMap.put(tree[j], j++);
             }
-
-            if(mapping.size() > 2) {
-                int min = trees.length-1;
-
-                for (int value : mapping.values()) {
+            if(fruitMap.size() > 2) {
+                int min = tree.length - 1;
+                for(int value : fruitMap.values()) {
                     min = Math.min(min, value);
                 }
-                start = min + 1;
-                mapping.remove(trees[min]);
+                i = min + 1;
+                fruitMap.remove(tree[min]);
             }
-            max = Math.max(max, current-start);
+            max = Math.max(max, j-i);
         }
+
+        return max;
     }
 }
